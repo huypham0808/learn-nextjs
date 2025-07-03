b77401a77e74202f4d2ccc2c20c4d4402d4965d4
huypham0808 <phamngohoanghuy95@gmail.com>
2025-07-03 23:02:24 +0700

    add logo to navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./help-desk_logo.png"; // Adjust the path as necessary
+
 export default function Navbar() {
     return (
         <nav>
+            {/* <h1>Help Desk App</h1> */}
+            <Image
+                src={Logo}
+                width={70}
+                quality={100}
+                placeholder="blur" />
+
             <Link href="/">Dash board</Link>
             <Link href="/tickets">Tickets</Link>
         </nav>
